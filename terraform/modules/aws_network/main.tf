3735b884d7dc78514da8f826621df50eb048836a
naturkach <60227593+naturkach@users.noreply.github.com>
2023-07-11 15:38:24 +0300

    Create main.tf

---
+++ terraform/modules/aws_network/main.tf
@@ -0,0 +1,104 @@
+# provider "aws" {             // no need- for module, is going to be in main tf of the project
+#   region = "eu-north-1"
+# }
+
+# get all available zones in this region
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.env}-vpc-l30"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+
+# public subnets and routing
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnets_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets_cidrs, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.env}-public-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${var.env}-route-public-subnets"
+  }
+}
+
+resource "aws_route_table_association" "public_routes" {
+  count          = length(aws_route_table.public_subnets[*].id)
+  route_table_id = aws_route_table.public_subnets.id
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+}
+
+
+# NAT GW for private ips with elastic ips
+
+resource "aws_eip" "nat" {
+  count  = length(var.private_subnet_cidrs)
+  domain = "vpc"
+  tags = {
+    Name = "${var.env}-nat-gw-${count.index + 1}"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  count         = length(var.private_subnet_cidrs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = element(aws_subnet.public_subnets[*].id, count.index)
+  tags = {
+    Name = "${var.env}-nat-gw-${count.index + 1}"
+  }
+}
+
+
+# ------------- private subnets
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "${var.env}-private-${count.index + 1}"
+  }
+}
+
+# each az has it's own natGW so we create route table for each:
+resource "aws_route_table" "private_subnets" {
+  count  = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+  tags = {
+    Name = "${var.env}-route-private-subnets-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private_routes" {
+  count          = length(aws_subnet.private_subnets[*].id)
+  route_table_id = aws_route_table.private_subnets[count.index].id
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+}
+
+#------------------------
